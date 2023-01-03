95e6acaffb9c9853a07c8f11efb3f442485b3143
Dalinte <kollekcionerssd1@gmail.com>
2023-01-03 17:50:30 +0300

    add docker (no hot reload)

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
+  devServer: {
+    client: {
+      webSocketURL: 'ws://0.0.0.0:8080/ws'
+    },
+    allowedHosts: 'all'
+  }
 })
